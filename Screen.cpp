b8cb6ed2ba51c0fc31e080abba73cd7b661d1adb
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2022-08-10 00:08:10 +0300

    initial commit

---
+++ Screen.cpp
@@ -0,0 +1,5 @@
+#include "Screen.h"
+
+String Screen::getText() {
+	return "Теперь можно";
+}
---
+++ Screen.h
@@ -0,0 +1,11 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <Arduino.h>
+
+class Screen {
+  public:
+	String getText();
+};
+
+#endif
